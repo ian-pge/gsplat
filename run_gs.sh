f1956df3b45b1d6ef570d06cf5e12f1e53d7df3b
ian <ian.page38@gmail.com>
2026-01-22 18:49:40 +0000

    udpate

--- run_gs.sh
+++ run_gs.sh
@@ -10,3 +10,15 @@ pixi run python /workspace/gsplat/examples/simple_trainer_2dgs.py \             
           --save_steps 50000 \
           --grow_grad2d 0.0001 \
           --test_every 1000
+
+
+pixi run python examples/simple_trainer.py default \
+        --data-dir /workspace/glomap2k_undistorted \
+        --random-bkgd \
+        --pose_opt \
+        --random_bkgd \
+        --use_fused_bilagrid \
+        --means_lr 0.000016 \
+        --opacity_reg 0.01 \
+        --scale_reg 0.01 \
+        --result-dir /workspace/gsplat/results/
