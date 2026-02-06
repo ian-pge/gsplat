5558b9794cc61f569b8475419fdbdc01f83915d3
Ian <82505982+ian-pge@users.noreply.github.com>
2026-02-06 11:17:16 +0100

    Update run_gs.sh

--- run_gs.sh
+++ run_gs.sh
@@ -3,13 +3,10 @@ pixi run python /workspace/gsplat/examples/simple_trainer_2dgs.py \             
           --data_factor 1 \
           --sh_degree 3 \
           --pose_opt \
-          --app_opt \
           --normal_loss \
           --dist_loss \
           --eval_steps 50000 \
-          --save_steps 50000 \
-          --grow_grad2d 0.0001 \
-          --test_every 1000
+          --save_steps 50000
 
 
 pixi run python examples/simple_trainer.py default \
